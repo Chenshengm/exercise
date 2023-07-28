32aa1d77ee444589ba91fb983d051b52af6be83e
Chenshengm <2359704636@qq.com>
2023-07-28 21:46:25 +0800

    Chenshengm:昨天比赛加上更上次比赛的D

---
+++ code/duality314/2023.7/2023.7.28/Monsters.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+typedef struct a {
+	int num;
+	int index;
+} a;
+
+bool cmp(const a& x, const a& y) {
+	if (x.num == y.num)
+		return x.index < y.index; // Choose the monster with smaller index if health is the same
+	return x.num > y.num;
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	while (N--) {
+		int n, k;
+		cin >> n >> k;
+		a* arr = new a[n]; // Use dynamic memory allocation
+
+		for (int i = 0; i < n; i++) {
+			int s;
+			cin >> s;
+			arr[i].num = s % k;
+			if (arr[i].num == 0)
+			{
+				arr[i].num = k;
+			}
+			arr[i].index = i + 1;
+		}
+		sort(arr, arr + n, cmp);
+
+		for (int i = 0; i < n; i++) {
+			cout << arr[i].index << " ";
+		}
+		cout << endl;
+
+		delete[] arr; // Deallocate the memory
+	}
+}
---
+++ code/duality314/2023.7/2023.7.28/Prefix Permutation Sums.cpp
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+/*https://codeforces.com/contest/1851/problem/D*/
+int main()
+{
+	int N;
+	cin >> N;
+l1:while (N--)
+{
+	int n;
+	cin >> n;
+	bool a[200002] = { false };
+	long long int s = 0;
+	long long int f = -1;
+	bool once = false;
+	bool no = false;
+	for (int i = 0; i < n - 1; i++)
+	{
+		long long int p;
+		cin >> p;
+		if (no)
+		{
+			continue;
+		}
+		if ((p - s) > n || a[p - s] == true)
+		{
+			if (once)
+			{
+				no = true;
+			}
+			f = p - s;
+			once = true;
+			s = p;
+			continue;
+		}
+		a[p - s] = true;
+		s = p;
+	}
+	if (no)
+	{
+		cout << "No" << endl;
+		goto l1;
+	}
+
+	long long int sum = 0;
+	int flag = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (!a[i])
+		{
+			sum += i;
+			flag++;
+		}
+	}
+	if ((flag != 2 || sum != f) && (!(flag == 1 && f == -1)))//考虑最后一个数丢失
+	{
+		cout << "No" << endl;
+		goto l1;
+	}
+	cout << "Yes" << endl;
+}
+}
